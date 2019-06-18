78c3f48ffed5bd9a76724f057fc9312fc75e3898
Archana Jha <archanadeveloper18@gmail.com>
2019-06-18 13:10:39 -0600

    Base Url

--- app/src/main/java/com/example/android/mybaking/API_Ingredients.java
+++ app/src/main/java/com/example/android/mybaking/API_Ingredients.java
@@ -7,6 +7,7 @@ import retrofit2.http.GET;
 
 public interface API_Ingredients {
 
+    //Base Url
     String BASE_URL="https://d17h27t6h515a5.cloudfront.net/";
 
     @GET("topher/2017/May/59121517_baking/baking.json")
